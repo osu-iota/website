40c9677b3c3ecf6e8b9d4a582ed43402f1b47f64
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-01-14 12:36:02 -0800

    adding responsiveness, bootstrap updates, contact form

--- calendar/index.php
+++ calendar/index.php
@@ -1,17 +1,20 @@
 <?php include '.meta.php' ?>
 <?php include BASE . '/include/templates/header.php'; ?>
-<div class="row">
-    <div class="col">
-        <h1>IOTA Calendar of Events</h1>
-    </div>
-</div>
-<div class="row ">
-    <div class="col calendar-col">
-        <div class="calendar">
-            <iframe src="https://calendar.google.com/calendar/embed?src=oregonstate.edu_hcoq3kf9giltkj2onl3t0l8f3c%40group.calendar.google.com&ctz=America%2FLos_Angeles" 
-                    style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    <div class="row">
+        <div class="col">
+            <h1>IOTA Calendar of Events</h1>
         </div>
     </div>
-</div>
+    <div class="row ">
+        <div class="col">
+            <div class="calendar">
+                <iframe class="iframe-calendar"
+                        src="https://calendar.google.com/calendar/embed?src=oregonstate.edu_hcoq3kf9giltkj2onl3t0l8f3c%40group.calendar.google.com&ctz=America%2FLos_Angeles"
+                        frameborder="0" scrolling="no"></iframe>
+
+            </div>
+
+        </div>
+    </div>
 
 <?php include BASE . '/include/templates/footer.php'; ?>
---
+++ contact/index.php
@@ -0,0 +1,39 @@
+<?php include_once '.meta.php' ?>
+<?php include_once BASE . '/include/templates/header.php' ?>
+<div class="row">
+    <div class="col">
+        <h1>Contact Us</h1>
+        <p>Use the form below to contact the OSU Internet of Things Alliance regarding any questions, comments, or
+            concerns you may have.</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <form method="POST" action="contact/submit.php">
+            <div class="form-row">
+                <div class="form-group col-4">
+                    <input class="form-control" type="text" name="name" placeholder="Enter name *" required/>
+                </div>
+                <div class="form-group col-4">
+                    <input class="form-control" type="email" name="email" placeholder="Enter email address *" required/>
+                </div>
+                <div class="form-group col-4">
+                    <input class="form-control" type="text" name="subject" placeholder="Enter message subject *"
+                           required/>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col">
+                    <textarea class="form-control" rows="10" placeholder="How can we help?" required></textarea>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col">
+                    <button class="btn btn-primary" type="submit">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+<?php include_once BASE . '/include/templates/footer.php' ?>
