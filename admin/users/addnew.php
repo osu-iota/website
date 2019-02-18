157940b532e43d4792eaafe6548de5b7f9d4547d
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-02-18 12:32:14 -0800

    implementing admin add user functionality

---
+++ admin/users/addnew.php
@@ -0,0 +1,30 @@
+<?php
+
+$onid = $_POST['onid'];
+$level = $_POST['privLevel'];
+
+if(empty($onid) || $onid == '' || (empty($level) && $level != 0) || !is_numeric($level)) {
+    $logger->error('Bad request when attempting to add new user with ONID ' . $onid . ' and level ' . $level);
+    http_response_code(400);
+    die();
+}
+
+$uid = \OSU\IOTA\Util\Security::generateSecureUniqueId();
+
+try {
+    $sql = 'INSERT INTO iota_user VALUES(:uid, :onid, :privLevel, :lastLogin)';
+    $prepared = $db->prepare($sql);
+    $prepared->bindParam(':uid', $uid, PDO::PARAM_STR);
+    $prepared->bindParam(':onid', $onid, PDO::PARAM_STR);
+    $prepared->bindParam(':privLevel', $level, PDO::PARAM_INT);
+    $prepared->bindParam(':lastLogin', time(), PDO::PARAM_INT);
+    $prepared->execute();
+
+} catch(PDOException $e) {
+    $logger->error($e->getMessage());
+    http_response_code(500);
+    die();
+}
+
+http_response_code(201);
+
--- admin/users/index.php
+++ admin/users/index.php
@@ -4,25 +4,24 @@ include_once '../menu.php';
 
 $levels = json_decode(file_get_contents(BASE . '/include/data/priv-levels.json'), true);
 
-$users = array();
-try {
-    $sql = 'SELECT * FROM iota_user ORDER BY u_onid'; // TODO: implement LIMIT for large amount of users
-    $users = $db->query($sql);
-} catch (PDOException $e) {
-    $logger->error($e->getMessage());
-}
-
 ?>
 <div class="admin-main">
     <div class="page">
-        <?php /*
         <div class="add-user">
-            <form onsubmit="return onAddUser()">
+            <form onsubmit="return onAddNewUser()">
                 <input required type="text" class="form-control" id="newUserOnid"
                        placeholder="Enter ONID for new user"/>
-                <button type="submit" class="btn btn-primary"><i class="fas fa-plus"></i>&nbsp;User</button>
+                <select required id="newUserLevel" class="form-control user-priv-select">
+                    <option value="">Select Level</option>
+                    <?php foreach ($levels as $index => $level): ?>
+                        <option value="<?php echo $index ?>">
+                            <?php echo $level ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+                <button type="submit" class="btn btn-primary" style="margin-left: 20px"><i class="fas fa-plus"></i>&nbsp;User</button>
             </form>
-        </div> */ ?>
+        </div>
         <table class="table">
             <thead>
             <th>ONID</th>
@@ -30,39 +29,30 @@ try {
             <th>Last Login</th>
             <th></th>
             </thead>
-            <tbody>
-            <?php foreach ($users as $user): ?>
-                <tr class="user">
-                    <td><?php echo $user['u_onid'] ?></td>
-                    <td>
-                        <select id="<?php echo $user['uid'] ?>" class="form-control user-priv-select"
-                                onChange="onPrivilegeLevelEdit(this)">
-                            <?php foreach ($levels as $index => $level): ?>
-                                <option value="<?php echo $index ?>" <?php if ($index == $user['u_privilege_level']) echo 'selected' ?>>
-                                    <?php echo $level ?>
-                                </option>
-                            <?php endforeach; ?>
-                        </select>
-                    </td>
-                    <td><?php echo date("F j, Y, g:i a", $user['u_last_login']) ?></td>
-                    <td></td>
-                </tr>
-            <?php endforeach; ?>
+            <tbody id="users">
+            <?php include_once 'loadusers.php' ?>
             </tbody>
         </table>
     </div>
 
 </div>
 <script>
-    function onPrivilegeLevelEdit(el) {
-        $.post('admin/users/update.php', {
-            id: el.id,
-            level: el.value
+    function onAddNewUser() {
+        var onid = document.getElementById('newUserOnid');
+        var level = document.getElementById('newUserLevel');
+
+        $.post('admin/users/addnew.php', {
+            onid: onid.value,
+            privLevel: level.value
         }).done(() => {
-            snackbar("Successfully updated the user's privilege level", 'success');
+            snackbar("Successfully created new user", 'success');
+            onid.value = '';
+            level.value = '';
+            $('#users').load('admin/users/loadusers.php');
         }).fail(() => {
-            snackbar("Failed to update the user's privilege level", 'error');
+            snackbar("Failed to create new user", 'error');
         });
+        return false;
     }
 </script>
 <?php include_once BASE . '/include/templates/footer.php'; ?>
