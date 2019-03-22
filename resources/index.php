db5a2a875837aa980e66fd2cbf8c25a117fe42c4
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-03-22 15:28:43 -0700

    fixing issue with not echoing topic html

--- resources/index.php
+++ resources/index.php
@@ -43,7 +43,7 @@ session_start();
         <h4>Topics</h4>
         <table class="table available-topics">
             <tbody id="topics">
-            <?php getTopicHtml($topics) ?>
+            <?php echo getTopicHtml($topics) ?>
             </tbody>
             <?php if ($userIsAdmin): ?>
                 <tfoot>
